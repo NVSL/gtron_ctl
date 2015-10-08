c30ceab390d436b2a7cc9f27da892d70ddd5c522
stevenjswanson <stevenjswanson@users.noreply.github.com>
2015-10-08 13:14:51 -0700

    support other users.

--- repo/bin/update_gadgetron.sh
+++ repo/bin/update_gadgetron.sh
@@ -12,11 +12,14 @@ pushd ${0%/*}
 
 source ../lib/install_util.sh
 
+user=$(cat ../config/bb_username.txt)
+echo "Checking under user: $user"
+
 confirm_gadgetron
 confirm_venv
 
 NVSL_GIT=git@github.com:NVSL
-NVSL_SVN=svn+ssh://swanson@bbfs-01.calit2.net/grw/Gordon/svn/branches/swanson/git-migration/Gadgets/
+NVSL_SVN=svn+ssh://$user@bbfs-01.calit2.net/grw/Gordon/svn/branches/swanson/git-migration/Gadgets/
 
 tools="
 $NVSL_SVN/Tools/CircuitsByCode
