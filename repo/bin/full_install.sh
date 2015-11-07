be189ab1b763a4b07cf8aabaf20d7bac94711876
Steven Swanson <swanson@cs.ucsd.edu>
2015-11-06 17:21:27 -0800

    Don't require github to work until we've set it up

--- repo/bin/full_install.sh
+++ repo/bin/full_install.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 
-git clone -b develop git@github.com:NVSL/gtron_devel.git
-pushd gtron_devel
 
-source repo/lib/install_util.sh
-source repo/lib/install_common.sh
-
-request "Enter your NVSL lab username:"
+echo  "Enter your NVSL lab username:"
 read nvsl
-request "Enter your github username:"
+echo "Enter your github username:"
 read github
 
 if ensure_ssh_key; then
@@ -18,6 +13,11 @@ fi
 
 start_ssh_agent
 
+git clone -b develop git@github.com:NVSL/gtron_devel.git
+pushd gtron_devel
+
+source repo/lib/install_util.sh
+source repo/lib/install_common.sh
 
 source gtron_env.sh
 banner "Ignore the following warnings..."
@@ -36,4 +36,4 @@ request "You need to do 'cd gtron_devel; source gtron_env.sh;'"
 
 request "Then you can type ' (cd Gadgets/Tools/jet_2/; make run)' to start jet."
 
-request "See https://sites.google.com/a/eng.ucsd.edu/gadgetron/getting-started/gadgetron-svn for more details about how to use the 'gtron' utility to manage this workspace."
+request "Type 'gtron full_docs' to learn how to use the 'gtron' utility to manage this workspace."
