6736be9b48542de19f21a1488b11b3b01b296401
Steven Swanson <swanson@cs.ucsd.edu>
2016-08-14 23:39:33 -0700

    new env script for setupified build

--- gtron_env.sh
+++ gtron_env.sh
@@ -2,7 +2,9 @@
 
 CDIR=`pwd`
 
-export GADGETRON_ROOT=$PWD/Gadgets
+. gtron_env_core.sh
+
+#export GADGETRON_ROOT=$PWD/Gadgets
 export GADGETRON_VENV=$PWD/repo/venv/Gadgetron
 
 export USE_VENV=yes
@@ -25,7 +27,6 @@ $GADGETRON_ROOT/Tools/Checkers/Checkers:\
 $GADGETRON_ROOT/Tools/GCompiler/GCompiler:\
 $GADGETRON_ROOT/Tools/CbC/bin:\
 /Applications/Arduino.app/Contents/MacOS:\
-$PWD/repo/bin:\
 $PATH
 
 export PATH
@@ -45,14 +46,10 @@ function deactivate_gadgetron () {
 
 activate_gadgetron
 
-export EAGLE_LIBS=$GADGETRON_ROOT/Libraries/GadgetronPCBLibs/Eagle
-export EAGLE_CAM=$GADGETRON_ROOT/Libraries/GadgetronCAM/Eagle
-export AUTOMAKIT=$GADGETRON_ROOT/Tools/Gadgetron
 export GADGETRON_CATALOG=$GADGETRON_ROOT/Libraries/JetComponents/Catalog
 export GADGETRON_CONFIG=$AUTOMAKIT/gadgetron.config
 export DOWNLOAD_DIR=$HOME/Downloads
 
-export GADGETRON_COMPONENT_LIB=$GADGETRON_ROOT/Libraries/JetComponents
 
 export PYTHONPATH=$GADGETRON_ROOT/../repo/bin:$PYTHONPATH
 
@@ -60,42 +57,7 @@ export PYTHONPATH=$GADGETRON_ROOT/../repo/bin:$PYTHONPATH
 # in order of preference
 # Don't include 7.5.0 unless you are also going to fix the order in that pass the .sch and .brd files to Eagle.  It has be in the right order or eagle will choke, and the correct order has changed in 7.5.0
 
-EAGLE_VERSIONS=( "7.6.0" "7.2.0" "7.3.0" "7.1.0" "7.0.1" "7.0.0" "7.4.0")
 
-# look for versions on on mac
-for i in "${EAGLE_VERSIONS[@]}"; do
-    p=/Applications/EAGLE-${i}/EAGLE.app/Contents/MacOS/EAGLE
-    echo "Looking for version" ${i}
-    if [ -e $p ]; then
-       echo "Found" $p
-       export EAGLE_EXE=$p
-       export EAGLE_DTD=/Applications/EAGLE-${i}/doc/eagle.dtd	
-       break;
-    fi
-done
-
-# look for versions on Linux
-for i in "${EAGLE_VERSIONS[@]}"; do
-    p=/opt/eagle-$i/bin/eagle
-    if [ -e $p ]; then
-        export EAGLE_EXE=$p
-        break;
-    fi
-done  
-
-# Look for eagle in your home directory.
-for i in "${EAGLE_VERSIONS[@]}"; do
-    p=$HOME/eagle-$i/bin/eagle
-    if [ -e $p ]; then
-        export EAGLE_EXE=$p
-        break;
-    fi
-done  
-
-#For the cluster
-if [ -e /gro/cad/eagle-7.2.0/bin/eagle ]; then
-    export EAGLE_EXE=/gro/cad/eagle-7.2.0/bin/eagle
-fi
 
 #For gcloud servers
 #if [ -e $(readlink ../eagle.sh) ]; then
